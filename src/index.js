bc42dd898ccf3ce394d3ce80c1b298806da5dd94
Unknown <aigucis@gmail.com>
2017-04-01 10:41:35 +0300

    reload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {app, BrowserWindow} from 'electron'
 import path from 'path'
 import url from 'url'
+import {enableLiveReload} from 'electron-compile';
+enableLiveReload();
 
 var appRoot = path.join(__dirname, '..');
 
